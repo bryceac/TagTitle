c0550ec1e868c9a9f065970f11bbb4db29b19fad
Bryce <tonyhawk2100@gmail.com>
2013-04-17 11:35:31 -0700

    change subtitle to make more sense

--- tagtitle.plugin.php
+++ tagtitle.plugin.php
@@ -7,7 +7,7 @@ class TagTitle extends Plugin
 		if ( Controller::get_action() == 'tag_collection' ) 
 		{ 
 			$xml->title =  ucwords(htmlentities(Tags::get_by_slug(Controller::get_var('tag'))->term_display)) . ' - ' . Utils::htmlspecialchars( Options::get( 'title' ) );
-			$xml->subtitle = htmlentities(Tags::get_by_slug(Controller::get_var('tag'))->term_display) . ' posts from ' . Utils::htmlspecialchars( Options::get( 'title' ) );
+			$xml->subtitle = ' posts on ' . htmlentities(Tags::get_by_slug(Controller::get_var('tag'))->term_display) . ' from ' . Utils::htmlspecialchars( Options::get( 'title' ) );
 		}
 	}
 }
